7263ada1969c67d960860a491874817b98180b5e
bing <sabine@Macintosh-8.local>
2019-11-25 20:08:31 -0800

    replace hardcode with props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,13 +4,16 @@ import classNames from 'classnames/bind';
 
 export default function InterviewerListItem(props) {
   return (
-    <li className="interviewers__item">
+    <li 
+      className="interviewers__item"
+      onClick={() => props.setInterviewer(props.name)}
+    >
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   )
 
