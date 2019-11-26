7914f9adef819928dd3b75efe95fb8dd97f4dda9
stella-zb <upperdaystudio@gmail.com>
2019-11-26 12:48:29 -0800

    import Empty / add button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
   return (
     <>
       <Header time={props.time} />
-      <article className="appointment"></article>
+      <article className="appointment">
+        <Empty onAdd={props.onAdd}/>
+      </article>
     </>
   );
 }
