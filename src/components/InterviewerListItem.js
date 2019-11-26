dbc363cdd41805e0893746fe29ab656748f17fd1
stella-zb <upperdaystudio@gmail.com>
2019-11-25 20:50:16 -0800

    conditional rendering for name that is selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,7 +6,7 @@ export default function InterviewerListItem(props) {
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected
   });
-
+  
   return (
     <li 
       className={interviewerClass}
@@ -17,7 +17,7 @@ export default function InterviewerListItem(props) {
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   );
 }
