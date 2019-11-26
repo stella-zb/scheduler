2928104e2543e4662a6ded2ada2afaa1db28cde8
stella-zb <upperdaystudio@gmail.com>
2019-11-26 12:37:09 -0800

    appointment component contain header component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <>
+      <Header time={props.time} />
+      <article className="appointment"></article>
+    </>
+  );
 }
