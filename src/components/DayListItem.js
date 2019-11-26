666937578445e70abdc790d0b61e3ec0841851aa
stella-zb <upperdaystudio@gmail.com>
2019-11-25 18:00:38 -0800

    rafactor with className, add add formatSpots function to generate the sentence, add test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "components/DayListItem.scss";
+import classNames from 'classnames/bind';
+import { render } from "@testing-library/react";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  const formatSpots = (
+    props.spots === 0? "no spots remaining" :
+    props.spots === 1? `${props.spots} spot remaining` : `${props.spots} spots remaining`
+  )
+
   return (
-    <li  onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots}</h3>
     </li>
   );
 }
