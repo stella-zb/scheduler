a7035fec73b58059f091219c85b68155b9ab8826
stella-zb <upperdaystudio@gmail.com>
2019-11-25 20:41:18 -0800

    map through interviewerList and apply child component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "components/InterviewerList.scss";
-import classNames from 'classnames/bind';
+import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
+  const interviewerList = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem 
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />    
+    );
+  });
   return (
     <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <h4 className="interviewers__header   text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerList}
+      </ul>
     </section>
-  )
-
+  );
 }
