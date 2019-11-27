34e4c90cb7a23057cb8e29ca26efe84a127ec2d0
bing <sabine@Macintosh-8.local>
2019-11-26 16:57:34 -0800

    conditional rendering for empty and show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,32 +1,40 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import { Fragment } from "react";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
+  const showOrEmpty = (
+    props.interview?
+    <Show 
+      student={props.interview.student}
+      interviewer={props.interview.interviewer.name}
+      onEdit={props.onEdit}
+      onDelete={props.onDelete}
+    /> : 
+    <Empty onAdd={props.onAdd}/>
+  )
+
+
   return (
-    <>
+    <Fragment>
       <Header time={props.time} />
-      <article className="appointment">
-        <Empty onAdd={props.onAdd}/>
-        <Show 
-          student={props.student}
-          interviewer={props.interviewer}
-          onEdit={props.onEdit}
-          onDelete={props.onDelete}
-        />
-        <Confirm 
+      <article className="appointment :last-of-type">
+        {showOrEmpty}
+        {/* <Confirm 
           message={props.message}
           onConfirm={props.onConfirm}
           onCancel={props.onCancel}
         />
         <Status message={props.message} />
-        <Error message={props.message} onClose={props.onClose} />
+        <Error message={props.message} onClose={props.onClose} /> */}
       </article>
-    </>
+    </Fragment>
   );
 }
