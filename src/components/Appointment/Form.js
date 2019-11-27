0c8c597084bfbce3eff36b08b9f570d7dc069abc
bing <sabine@Macintosh-8.local>
2019-11-26 16:32:21 -0800

    reset input function, pass into cancel function when props.onCancel gets called, prevent default submit for form when press enter, pass in data for the onSave and make it as callback when onClick event occur

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,10 +8,20 @@ export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    props.onCancel();
+    reset();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             value={name}
@@ -28,8 +38,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
