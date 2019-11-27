2aa8a0d98fcde9621f73e7824b44def1b721b4f3
stella-zb <upperdaystudio@gmail.com>
2019-11-27 15:05:48 -0800

    function to give back the appointments of given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export function getAppointmentsForDay(state, day) {
+  const appointmentDay = state.days.find(obj => {
+    if (obj.name === day) {
+      return obj.appointments
+    }
+  }) 
+  if (!appointmentDay) { return [] }
+  return appointmentDay.appointments.map( dayNum => state.appointments[dayNum] )
+}
