9d2fcbd517041ed9e97e237b21a91e95c3d6d851
stella-zb <upperdaystudio@gmail.com>
2019-11-27 17:47:05 -0800

    function return the interview detail info

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   const appointmentDay = state.days.find(obj => {
     if (obj.name === day) {
       return obj.appointments
@@ -6,4 +6,16 @@ export default function getAppointmentsForDay(state, day) {
   }) 
   if (!appointmentDay) { return [] }
   return appointmentDay.appointments.map( dayNum => state.appointments[dayNum] )
+}
+
+export function getInterview(state, interview) {
+  let result = {};
+  for (const i in state.appointments) {
+    if (state.appointments[i].interview === interview) {
+      if(!state.appointments[i].interview) return null
+      result.student = interview.student
+      result.interviewer = state.interviewers[interview.interviewer]
+    }
+  }
+  return result
 }
