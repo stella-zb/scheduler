87957849f7f9628807834deb4666961d964a5f50
bing <sabine@Macintosh-8.local>
2019-11-27 16:29:58 -0800

    fix syntax

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const appointmentDay = state.days.find(obj => {
     if (obj.name === day) {
       return obj.appointments
