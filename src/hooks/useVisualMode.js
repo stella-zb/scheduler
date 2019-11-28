7ff09980a7a556ee4b7c673132b55269cd86560b
bing <sabine@Macintosh-8.local>
2019-11-27 21:35:00 -0800

    building hook, setup for initial mode and story mode in the history array; when there is new input coming, the transition function that will update with new mode, if replace needed, update the new history by adding up the new mode into the history array; when back function get call, if there is only one item in the array, stop, otherwise remove the last item in history array, update the mode with that last item of current history array

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export default function useVisualMode(initial) {
+  const [mode, setMode]=useState(initial)
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    // update the mode with the given mode
+    setMode(newMode)  
+    // if replace is false which mean true
+    // update the history with the given mode
+    if (replace === false)
+    setHistory(history.concat(newMode))
+  }
+
+  const back = () => {
+    // remove the last item in history array
+    history.pop()
+    // check if history array length is greater than 1 to keep the initial mode
+    if (history.length < 1) return
+    // update Mode with the last element in history array
+    setMode(history[history.length - 1])
+    // set newHistory after previous action ?
+    // setHistory(history) ?
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  }
+}
