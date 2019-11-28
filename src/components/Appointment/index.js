1a6f8d7c05d63d2fef7b5ca8a79b35da783df833
bing <sabine@Macintosh-8.local>
2019-11-28 12:54:10 -0800

    import custom Hooks, conditional rendering for SHOW and EMPTY mode,  add a callback of transition(CREATE) to onAdd button on Empty mode, and transition(EMPTY) to onCancel on Form mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
-import { Fragment } from "react";
+
+import useVisualMode from "hooks/useVisualMode";
+
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
-import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
-  const showOrEmpty = (
-    props.interview? 
-    <Show
-      student={props.interview.student}
-      interviewer={props.interview.interviewer.name}
-      onEdit={props.onEdit}
-      onDelete={props.onDelete}
-    /> : 
-    <Empty onAdd={props.onAdd}/>
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
   );
 
   return (
     <Fragment>
       <Header time={props.time} />
       <article className="appointment">
-        {showOrEmpty}
+        {mode === EMPTY && 
+          <Empty 
+            onAdd={ () => transition(CREATE) } 
+          />
+        }
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer.name}
+            onEdit={props.onEdit}
+            onDelete={props.onDelete}
+          />
+        )}
+        {mode === CREATE && 
+          <Form 
+            onCancel={ () => transition(EMPTY) }
+          />
+        }
         {/* <Confirm 
           message={props.message}
           onConfirm={props.onConfirm}
