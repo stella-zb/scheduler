d68cbc27be7e0ef0640e73381920524090ef1c12
stella-zb <upperdaystudio@gmail.com>
2019-11-28 23:28:41 -0800

    save function that take in name and interviewer info from form and props into bookInterview function, transition the SHOW mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -19,6 +19,16 @@ export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+  
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name, 
+      interviewer
+    };
+    
+    props.bookInterview(props.id, interview) 
+    transition(SHOW)
+  }
 
   return (
     <Fragment>
@@ -39,8 +49,9 @@ export default function Appointment(props) {
         )}
         {mode === CREATE && 
           <Form 
-            interviewer={ [] }
-            onCancel={ () => transition(EMPTY) }
+            interviewers={ props.interviewers }
+            onCancel={ () => back() }
+            onSave={ save }
           />
         }
         {/* <Confirm 
