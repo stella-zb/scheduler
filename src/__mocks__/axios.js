90f1a1de606d8cd6b6c275ddc680a4561f93c4d1
bing <sabine@Macintosh-8.local>
2019-12-02 21:03:13 -0800

    give the default url address

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -55,7 +55,7 @@ const fixtures = {
 
 export default {
   get: jest.fn(url => {
-    if (url === "/api/days") {
+    if (url === "http://localhost:8001/api/days") {
       return Promise.resolve({
         status: 200,
         statusText: "OK",
@@ -63,7 +63,7 @@ export default {
       });
     }
 
-    if (url === "/api/appointments") {
+    if (url === "http://localhost:8001/api/appointments") {
       return Promise.resolve({
         status: 200,
         statusText: "OK",
@@ -71,7 +71,7 @@ export default {
       })
     }
 
-    if (url === "/api/interviewers") {
+    if (url === "http://localhost:8001/api/interviewers") {
       return Promise.resolve({
         status: 200,
         statusText: "OK",
