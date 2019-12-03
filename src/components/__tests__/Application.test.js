9401e4a3e8f66a334258acc051e981cfdf314a55
bing <sabine@Macintosh-8.local>
2019-12-02 21:02:15 -0800

    jest.mock(...) function to mock the axios module without actually hitting the API, add fireEvent for click on Tuesday

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
 
-  return waitForElement(() => getByText("Monday"));
-});
+  beforeEach(() => {
+    jest.mock("axios", () => require("../../__mocks__/axios"));
+  })
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+    
+    return waitForElement(() => getByText("Monday"))
+      .then(() => {
+        fireEvent.click(getByText("Tuesday"));
+        expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      })
+  });
+})
