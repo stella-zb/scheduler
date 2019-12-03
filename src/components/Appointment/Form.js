8db97993b8209b96970b486da86fca875b6ae773
stella-zb <upperdaystudio@gmail.com>
2019-12-02 18:50:34 -0800

    add error state, create validate function to check if there is name input, giving error message if not, otherwise, call onSave and pass in name, interviewer data

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
@@ -18,6 +19,13 @@ export default function Form(props) {
     reset();
   };
 
+  const validate = () => {
+    if(name === "") {
+      return setError("Student name cannot be blank");
+    }
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,8 +36,12 @@ export default function Form(props) {
             onChange={event => setName(event.target.value)}
             type="text"
             placeholder="Enter Student Name"
+            data-testid = "student-name-input"
           />
         </form>
+
+        <section className="appointment__validation">{error}</section>
+
         <InterviewerList
           interviewers={props.interviewers}
           interviewer={interviewer} 
@@ -40,7 +52,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={() => validate()}>Save</Button>
         </section>
       </section>
     </main>
