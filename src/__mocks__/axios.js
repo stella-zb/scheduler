0a8e26b044f70232c5e347b48a46fcf816696837
bing <sabine@Macintosh-8.local>
2019-12-04 00:52:11 -0800

    fake delete url api request for test

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -99,5 +99,15 @@ export default {
       status: 404,
       statusText: "404 Error"
     })
+  }),
+
+  delete: jest.fn(url => {
+    if (url === "/api/appointments/2") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content",
+        data: fixtures.appointments
+      })
+    }
   })
 }
