33837236aa36ebe314f862d8ca8818bf5e01339d
stella-zb <upperdaystudio@gmail.com>
2019-12-04 00:52:44 -0800

    test for cancel a interview with spots update

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -43,4 +43,24 @@ describe("Application", () => {
     
   });
 
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[1];
+
+    fireEvent.click(getByAltText(appointment, "Delete"));
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+  
+    await waitForElement(() => getByAltText(appointment, "Add"));
+    
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+    debug(day)
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+
+  })
 })
