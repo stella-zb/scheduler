cbe1f3a3d1293e613a608ab5c9b8a348dc6f4d61
bing <sabine@Macintosh-8.local>
2019-12-04 20:16:41 -0800

    cypress test check name after booked and rafactor

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,10 +1,12 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset")
-
     cy.visit("/");
     cy.contains("Monday");
 
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
@@ -12,5 +14,8 @@ describe("Appointments", () => {
     cy.get("[data-testid=student-name-input").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 });
