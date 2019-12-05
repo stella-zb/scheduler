0d39cd3e38fdba7167b434bf97f58d5cc77ec99b
bing <sabine@Macintosh-8.local>
2019-12-04 20:32:04 -0800

    cypress test for editing an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -18,4 +18,15 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+    cy.get("[data-testid=student-name-input").clear().type("Stella");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Stella");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
