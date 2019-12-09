d72c6acb93669bcb55e82f8d4a100a87f1eec472
bing <sabine@Macintosh-8.local>
2019-12-09 13:33:10 -0800

    rafactor

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,14 @@
 export function getAppointmentsForDay(state, dayName) {
   const targetDay = state.days.find(day => day.name === dayName)
   if (!targetDay) { return [] }
-  return targetDay.appointments.map( dayNum => state.appointments[dayNum] )
+  return targetDay.appointments.map((dayNum) => state.appointments[dayNum] || [])
 }
 
 export function getInterview(state, interview) {
   let result = {};
   for (const i in state.appointments) {
     if (state.appointments[i].interview === interview) {
-      if(!state.appointments[i].interview) return null
+      if (!state.appointments[i].interview) return null
       result.student = interview.student
       result.interviewer = state.interviewers[interview.interviewer]
     }
@@ -17,7 +17,7 @@ export function getInterview(state, interview) {
 }
 
 export function getInterviewersForDay(state, dayName) {
-  const targetDay = state.days.find(day => day.name === dayName) 
+  const targetDay = state.days.find(day => day.name === dayName)
   if (!targetDay) { return [] }
-  return targetDay.interviewers.map( dayNum => state.interviewers[dayNum] )
+  return targetDay.interviewers.map((dayNum) => state.interviewers[dayNum] || [])
 }
